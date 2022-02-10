a23669aac660754d9b4c4dd55f2bb60db064b103
Stepan Snigirev <snigirev.stepan@gmail.com>
2022-02-10 16:44:31 +0100

    use flask

---
+++ amp.py
@@ -0,0 +1,148 @@
+import os
+from typing import Tuple
+import requests
+import json
+
+def list2dict(arr:list, idkey:str='id', cls=None, args=[]) -> dict:
+    """Converts a list to dict using `idkey` field as key in the dict"""
+    return { a[idkey]: cls(*args, **a) if cls else a for a in arr }
+
+class APIException(Exception):
+    def __init__(self, msg, code):
+        self.msg = msg
+        self.code = code
+    
+    def __str__(self):
+        return f"Error {self.code}: {self.msg}"
+
+class Asset(dict):
+    def __init__(self, amp, **kwargs):
+        self.amp = amp
+        self._summary = {}
+        self._assignments = None
+        self._distributions = None
+        self._lost_outputs = None
+        super().__init__(**kwargs)
+
+    @property
+    def summary(self):
+        if not self._summary:
+            self._summary = self.amp.fetch_json(f"/assets/{self.asset_uuid}/summary")
+        return self._summary
+
+    @property
+    def assignments(self):
+        if self._assignments is None:
+            self._assignments = self.amp.fetch_json(f"/assets/{self.asset_uuid}/assignments")
+        return self._assignments or []
+
+    @property
+    def pending_assignments(self):
+        return [ass for ass in self.assignments if not ass['is_distributed']]
+
+    @property
+    def distributions(self):
+        if self._distributions is None:
+            self._distributions = self.amp.fetch_json(f"/assets/{self.asset_uuid}/distributions")
+        return self._distributions or []
+
+    @property
+    def unconfirmed_distributions(self):
+        return [dist for dist in self.distributions if dist['distribution_status'] == 'UNCONFIRMED']
+
+    @property
+    def lost_outputs(self):
+        if self._lost_outputs is None:
+            self._lost_outputs = self.amp.fetch_json(f"/assets/{self.asset_uuid}/lost-outputs")
+        return self._lost_outputs
+
+    @property
+    def asset_uuid(self):
+        return self['asset_uuid']
+
+    @property
+    def id(self):
+        return self.asset_uuid
+
+    @property
+    def name(self):
+        return self['name']
+
+    @property
+    def ticker(self):
+        return self['ticker']
+
+    @property
+    def asset_id(self):
+        return self['asset_id']
+
+    @property
+    def transfer_restricted(self):
+        return self['transfer_restricted']
+    
+    @property
+    def asset_type(self):
+        return 'transfer restricted' if self.transfer_restricted else 'trackable'
+
+    @property
+    def status(self):
+        s = []
+        for k in ["is_registered", "is_authorized", "is_locked"]:
+            s.append(k.replace("is_", "" if self[k] else "not "))
+        return ", ".join(s)
+
+    @property
+    def categories(self):
+        return {cat: self.amp.categories[cat] for cat in self['requirements']}
+
+class Amp:
+    def __init__(self, api, auth) -> None:
+        self.api = api
+        self.auth = auth
+        self.assets = {}
+        self.categories = {}
+        self.users = {}
+        self.managers = {}
+
+    @property
+    def headers(self) -> dict:
+        return {'content-type': 'application/json', 'Authorization': self.auth}
+
+    def fetch(self, path:str, method="GET", data=None) -> Tuple[str, int]:
+        path = path.lstrip("/")
+        fpath = f"public/api/{path}.json"
+        # return cached version
+        if method == "GET":
+            if os.path.isfile(fpath):
+                print(f"cached {path}")
+                with open(fpath, "r") as f:
+                    return f.read(), 200
+
+        api_url = f"{self.api}{path}"
+        params = dict(headers=self.headers)
+        if data:
+            params["data"] = data
+        print(f"fetch {path}")
+        res = requests.request( method, api_url, **params)
+        try:
+            os.makedirs(os.path.dirname(fpath))
+        except:
+            pass
+        try:
+            with open(fpath, "w") as f:
+                f.write(res.text)
+        except:
+            pass
+        return res.text, res.status_code
+
+    def fetch_json(self, path:str, method="GET", data=None):
+        txt, code = self.fetch(path, method, data)
+        if code < 200 or code > 299:
+            raise APIException(txt, code)
+        return json.loads(txt)
+
+    def sync(self):
+        self.users = list2dict(self.fetch_json("/registered_users"))
+        self.assets = list2dict(self.fetch_json("/assets"), 'asset_uuid', cls=Asset, args=[self])
+        self.categories = list2dict(self.fetch_json("/categories"))
+        self.managers = list2dict(self.fetch_json("/managers"))
--- serv.py
+++ serv.py
@@ -1,43 +1,78 @@
-from flask import Flask, send_from_directory, request
-from flask_cors import CORS
-import os
-import requests
+from flask import Flask, render_template, request
+from amp import Amp
 
 app = Flask(__name__)
-CORS(app)
 
 API_URL = "https://amp-test.blockstream.com/api/"
 AUTH = "token 66b946392661e192718a8822046a1e9a9dc7af51"
 
-@app.route('/api/<path:path>', methods=["GET", "POST", "PUT", "DELETE"])
-def api(path):
-    path = path.lstrip("/")
-    fpath = f"public/api/{path}.json"
-    # return cached version
-    if request.method == "GET":
-        if os.path.isfile(fpath):
-            print(f"cached {path}")
-            with open(fpath, "r") as f:
-                return f.read()
+amp = Amp(API_URL, AUTH)
 
-    api_url = f"{API_URL}{path}"
-    headers = {'content-type': 'application/json', 'Authorization': AUTH}
-    data = request.data
-    params = dict(headers=headers)
-    if data:
-        params["data"] = data
-    print(f"fetch {path}")
-    res = requests.request( request.method, api_url, **params)
-    try:
-        os.makedirs(os.path.dirname(fpath))
-    except:
-        pass
-    try:
-        with open(fpath, "w") as f:
-            f.write(res.text)
-    except:
-        pass
-    return res.text, res.status_code
+@app.route("/")
+def index():
+    return render_template('dashboard.jinja', amp=amp)
+
+
+
+
+@app.route("/assets/")
+def assets():
+    return render_template('assets.jinja', amp=amp)
+
+@app.route("/assets/<asset_uuid>/")
+def asset(asset_uuid):
+    return render_template('base.jinja', amp=amp)
+
+@app.route("/new_asset/")
+def new_asset():
+    return render_template('base.jinja', amp=amp)
+
+
+
+
+@app.route("/categories/")
+def categories():
+    return render_template('base.jinja', amp=amp)
+
+@app.route("/categories/<cid>/")
+def category(cid):
+    return render_template('base.jinja', amp=amp)
+
+
+
+
+@app.route("/users/")
+def users():
+    return render_template('base.jinja', amp=amp)
+
+
+
+
+@app.route("/managers/")
+def managers():
+    return render_template('base.jinja', amp=amp)
+
+
+
+
+@app.route("/treasury/")
+def treasury():
+    return render_template('base.jinja', amp=amp)
+
+
+
+
+@app.route("/settings/")
+def settings():
+    return render_template('base.jinja', amp=amp)
+
+
+
+
+@app.route("/api/<path:path>", methods=["GET", "POST", "PUT", "DELETE"])
+def api(path):
+    return amp.fetch(path, request.method, request.data)
 
 if __name__ == "__main__":
+    amp.sync()
     app.run(debug=True, port=8081)
